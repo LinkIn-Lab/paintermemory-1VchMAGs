4e8054984f5e571d5e752959821c0bbd5694cd62
linkinlab <153810687+linkinlab@users.noreply.github.com>
2024-12-21 00:54:22 +0530

    mompmpmppm

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "vpc" {
 
   name = "scoutflo-vpc-${random_string.suffix.result}"
 
-  cidr = "10.0.0.0/16"
+  cidr = "0.0.0.0/0"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
   #private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
